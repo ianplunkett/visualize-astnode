a096f765106a7436f53066e6edeafd0e677ec1c2
Ian Plunkett <ian.plunkett@iplunk-mb13r.attlocal.net>
2015-08-08 09:35:41 -0700

    Pulling data from external file

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
-const data      = [1, 4, 8, 15, 16, 23, 42],
-      width     = 420,
+const width     = 420,
       barHeight = 20;
 
 const x = d3.scale.linear()
-        .domain([0, d3.max(data)])
-        .range([0, 420]);
+        .range([0, width]);
 
 const chart = d3.select(".chart")
-        .attr("width", width)
-        .attr("height", barHeight * data.length);
+          .attr("width", width);
 
-const bar = chart.selectAll("g")
-          .data(data)
-          .enter().append("g")
-          .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")";});
+d3.tsv("http://0.0.0.0:8080/data.tsv", type, function(error, data) {
+    x.domain([0, d3.max(data, function(d) {return d.value;})]);
 
-bar.append("rect")
-    .attr("width", x)
-    .attr("height", barHeight - 1);
+    chart.attr("height", barHeight * data.length);
 
-bar.append("text")
-    .attr("x", function(d) {return x(d) - 3;})
-    .attr("y", barHeight / 2)
-    .attr("dy", ".35em")
-    .text(function(d) {return d;});
+    const bar = chart.selectAll("g")
+              .data(data)
+              .enter().append("g")
+              .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")";});
+    bar.append("rect")
+        .attr("width", function(d) {return x(d.value);})
+        .attr("height", barHeight - 1);
+
+    bar.append("text")
+        .attr("x", function(d) {return x(d.value) - 3;})
+        .attr("y", barHeight / 2)
+        .attr("dy", ".35em")
+        .text(function(d) {return d.value;});
+    
+});
+
+function type(d) {
+    d.value = +d.value;
+    return d;
+}

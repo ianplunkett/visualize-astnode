6a0f8fdc8ea6caeae1e6c016585a944130a6d6cb
Ian Plunkett <ian.plunkett@iplunk-mb13r.attlocal.net>
2015-08-08 08:45:06 -0700

    Chart coded in svg+d3

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
-var data = [1, 4, 8, 15, 16, 23, 42];
-d3.selectAll("section")
-    .attr("class", "special")
-    .append("div")
-    .html("Hello, world!!");
+const data      = [1, 4, 8, 15, 16, 23, 42],
+      width     = 420,
+      barHeight = 20;
 
-var section = d3.selectAll("section");
-
-section.append("div")
-    .html("First!");
-
-section.append("div")
-    .html("Second.");
-
-var x = d3.scale.linear()
+const x = d3.scale.linear()
         .domain([0, d3.max(data)])
         .range([0, 420]);
 
-d3.select(".chart")
-    .selectAll("div")
-    .data(data)
-    .enter().append("div")
-    .style("width", function(d) { return x(d) + "px";})
-    .text(function(d) { return d; });
+const chart = d3.select(".chart")
+        .attr("width", width)
+        .attr("height", barHeight * data.length);
+
+const bar = chart.selectAll("g")
+          .data(data)
+          .enter().append("g")
+          .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")";});
+
+bar.append("rect")
+    .attr("width", x)
+    .attr("height", barHeight - 1);
+
+bar.append("text")
+    .attr("x", function(d) {return x(d) - 3;})
+    .attr("y", barHeight / 2)
+    .attr("dy", ".35em")
+    .text(function(d) {return d;});
